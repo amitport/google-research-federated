9ce60f57a710836d76c85e18370310a5125b3ea9
yoavt <yoavti@post.bgu.ac.il>
2020-11-11 12:33:02 +0200

    added NumExamplesCounter to metrics_builder

--- optimization/emnist/federated_emnist.py
+++ optimization/emnist/federated_emnist.py
@@ -20,6 +20,7 @@ from absl import logging
 import tensorflow as tf
 import tensorflow_federated as tff
 
+from optimization.shared.keras_metrics import NumExamplesCounter
 from utils import training_loop
 from utils import training_utils
 from utils.datasets import emnist_dataset
@@ -29,19 +30,19 @@ EMNIST_MODELS = ['cnn', '2nn']
 
 
 def run_federated(
-    iterative_process_builder: Callable[..., tff.templates.IterativeProcess],
-    client_epochs_per_round: int,
-    client_batch_size: int,
-    clients_per_round: int,
-    max_batches_per_client: Optional[int] = -1,
-    client_datasets_random_seed: Optional[int] = None,
-    model: Optional[str] = 'cnn',
-    total_rounds: Optional[int] = 1500,
-    experiment_name: Optional[str] = 'federated_emnist_cr',
-    root_output_dir: Optional[str] = '/tmp/fed_opt',
-    max_eval_batches: Optional[int] = None,
-    **kwargs):
-  """Runs an iterative process on the EMNIST character recognition task.
+        iterative_process_builder: Callable[..., tff.templates.IterativeProcess],
+        client_epochs_per_round: int,
+        client_batch_size: int,
+        clients_per_round: int,
+        max_batches_per_client: Optional[int] = -1,
+        client_datasets_random_seed: Optional[int] = None,
+        model: Optional[str] = 'cnn',
+        total_rounds: Optional[int] = 1500,
+        experiment_name: Optional[str] = 'federated_emnist_cr',
+        root_output_dir: Optional[str] = '/tmp/fed_opt',
+        max_eval_batches: Optional[int] = None,
+        **kwargs):
+    """Runs an iterative process on the EMNIST character recognition task.
 
   This method will load and pre-process dataset and construct a model used for
   the task. It then uses `iterative_process_builder` to create an iterative
@@ -89,63 +90,65 @@ def run_federated(
       `federated_research/utils/training_utils.py`.
   """
 
-  emnist_train, _ = emnist_dataset.get_emnist_datasets(
-      client_batch_size,
-      client_epochs_per_round,
-      max_batches_per_client=max_batches_per_client,
-      only_digits=False)
+    emnist_train, _ = emnist_dataset.get_emnist_datasets(
+        client_batch_size,
+        client_epochs_per_round,
+        max_batches_per_client=max_batches_per_client,
+        only_digits=False)
 
-  _, emnist_test = emnist_dataset.get_centralized_datasets(
-      train_batch_size=client_batch_size,
-      max_test_batches=max_eval_batches,
-      only_digits=False)
+    _, emnist_test = emnist_dataset.get_centralized_datasets(
+        train_batch_size=client_batch_size,
+        max_test_batches=max_eval_batches,
+        only_digits=False)
 
-  input_spec = emnist_train.create_tf_dataset_for_client(
-      emnist_train.client_ids[0]).element_spec
+    input_spec = emnist_train.create_tf_dataset_for_client(
+        emnist_train.client_ids[0]).element_spec
 
-  if model == 'cnn':
-    model_builder = functools.partial(
-        emnist_models.create_conv_dropout_model, only_digits=False)
-  elif model == '2nn':
-    model_builder = functools.partial(
-        emnist_models.create_two_hidden_layer_model, only_digits=False)
-  else:
-    raise ValueError(
-        'Cannot handle model flag [{!s}], must be one of {!s}.'.format(
-            model, EMNIST_MODELS))
+    if model == 'cnn':
+        model_builder = functools.partial(
+            emnist_models.create_conv_dropout_model, only_digits=False)
+    elif model == '2nn':
+        model_builder = functools.partial(
+            emnist_models.create_two_hidden_layer_model, only_digits=False)
+    else:
+        raise ValueError(
+            'Cannot handle model flag [{!s}], must be one of {!s}.'.format(
+                model, EMNIST_MODELS))
 
-  loss_builder = tf.keras.losses.SparseCategoricalCrossentropy
-  metrics_builder = lambda: [tf.keras.metrics.SparseCategoricalAccuracy()]
+    loss_builder = tf.keras.losses.SparseCategoricalCrossentropy
 
-  def tff_model_fn() -> tff.learning.Model:
-    return tff.learning.from_keras_model(
-        keras_model=model_builder(),
-        input_spec=input_spec,
-        loss=loss_builder(),
-        metrics=metrics_builder())
+    def metrics_builder():
+        return [tf.keras.metrics.SparseCategoricalAccuracy(), NumExamplesCounter()]
 
-  training_process = iterative_process_builder(tff_model_fn)
+    def tff_model_fn() -> tff.learning.Model:
+        return tff.learning.from_keras_model(
+            keras_model=model_builder(),
+            input_spec=input_spec,
+            loss=loss_builder(),
+            metrics=metrics_builder())
 
-  client_datasets_fn = training_utils.build_client_datasets_fn(
-      train_dataset=emnist_train,
-      train_clients_per_round=clients_per_round,
-      random_seed=client_datasets_random_seed)
+    training_process = iterative_process_builder(tff_model_fn)
 
-  evaluate_fn = training_utils.build_evaluate_fn(
-      eval_dataset=emnist_test,
-      model_builder=model_builder,
-      loss_builder=loss_builder,
-      metrics_builder=metrics_builder)
+    client_datasets_fn = training_utils.build_client_datasets_fn(
+        train_dataset=emnist_train,
+        train_clients_per_round=clients_per_round,
+        random_seed=client_datasets_random_seed)
 
-  logging.info('Training model:')
-  logging.info(model_builder().summary())
+    evaluate_fn = training_utils.build_evaluate_fn(
+        eval_dataset=emnist_test,
+        model_builder=model_builder,
+        loss_builder=loss_builder,
+        metrics_builder=metrics_builder)
 
-  training_loop.run(
-      iterative_process=training_process,
-      client_datasets_fn=client_datasets_fn,
-      validation_fn=evaluate_fn,
-      test_fn=evaluate_fn,
-      total_rounds=total_rounds,
-      experiment_name=experiment_name,
-      root_output_dir=root_output_dir,
-      **kwargs)
+    logging.info('Training model:')
+    logging.info(model_builder().summary())
+
+    training_loop.run(
+        iterative_process=training_process,
+        client_datasets_fn=client_datasets_fn,
+        validation_fn=evaluate_fn,
+        test_fn=evaluate_fn,
+        total_rounds=total_rounds,
+        experiment_name=experiment_name,
+        root_output_dir=root_output_dir,
+        **kwargs)
